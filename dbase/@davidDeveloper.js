19588e16ba8f941fe108438bd5a0ecf71471cffe
ᴄʀᴀᴢʏXʏxx <davidjnior2000@gmail.com>
2025-04-03 22:04:59 +0100

    Add files via upload

---
+++ dbase/@davidDeveloper.js
@@ -0,0 +1,11 @@
+/* je suis un apprenti développeur fullstack cette application était au départ un test mais je compte bien en faire mon gagne pain de demain 
+
+- Backend ( ajouter et créé par moi David alias (crazy Dev))
+- Frontend (créé par moi et corrigé par GPT-4)*/
+
+
+require CrazyDev = false ;
+
+console.error{ =>((
+console.log('more learning for more informations')
+};
